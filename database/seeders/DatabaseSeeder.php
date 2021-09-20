7ae34fd8d95626bc64419694267e75e270dc23ff
iglebekk <anders@iglebekk.no>
2021-09-20 15:36:16 +0200

    cleaned up and made a front view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'Anders',
+            'email' => 'anders@tolvtemann.no',
+            'password' => Hash::make('password'),
+            'supadm' => 1
+        ]);
     }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<center>
+    <h1>Invitte</h1>
+    <a href="#" class="btn btn-sm btn-light">Login</a>
+</center>
+
+@endsection
