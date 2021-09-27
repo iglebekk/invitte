834a3dddf2cb631b4597cfe7d98ee8ec282fbb77
iglebekk <anders@iglebekk.no>
2021-09-27 20:22:29 +0200

    removed dev stuff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,11 +8,11 @@
     @csrf
     <div class="mb-3">
       <label for="email" class="form-label">E-postadresse</label>
-      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required value="anders@tolvtemann.no">
+      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required>
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Passord</label>
-      <input type="password" class="form-control" id="password" name="password" required value="password">
+      <input type="password" class="form-control" id="password" name="password" required>
     </div>
     <button type="submit" class="btn btn-primary">Logg inn</button>
   </form>
